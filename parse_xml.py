c6c1d4c4e64e90bc5d194b1a5f903900bc729337
Matt McNally <mgmcnally@umass.edu>
2015-11-11 17:41:19 -0500

    slight clean ups

--- parse_xml.py
+++ parse_xml.py
@@ -10,26 +10,17 @@ infile = STATIC_PATH+'/datasets/Users.xml'
 context = etree.iterparse(infile)
 
 def parse_block(elem):
-    # print("printing elem")
-    # print(elem.tag)
-    # print elem.tag, elem.attrib['DownVotes'
     for key,value in elem.attrib.iteritems():
         print "%s    %s" % (key,value)
     print("")
 
 def fast_iter(context):
     ct = 0
-    # print("starting fast iterator")
-    # print("printing context...")
-    # print(context)
-
     for event, elem in context:
         if(ct > 10):
             break
-        #print("count is %d" % ct)
         ct += 1
 
-        #print("about to call parse_block")
         parse_block(elem)
         elem.clear()
         while elem.getprevious() is not None:
