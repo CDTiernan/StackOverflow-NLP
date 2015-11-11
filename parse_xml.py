8aa4f0459d25c8ac3683c07de4463ebb4f65d7f4
Carter Tiernan <ctiernan@umass.edu>
2015-11-11 17:38:00 -0500

    working towards creating data objects from dataset

--- parse_xml.py
+++ parse_xml.py
@@ -8,31 +8,28 @@ infile = STATIC_PATH+'/datasets/Users.xml'
 
 # context = etree.iterparse(infile, events=('end',), tag='DisplayName')
 context = etree.iterparse(infile)
-ct = 0
-
-
 
 def parse_block(elem):
-    print("printing elem")
-    print(elem.tag)
-    print elem.tag, elem.attrib['DownVotes']
-    for thing in elem:
-        print(thing)
+    # print("printing elem")
+    # print(elem.tag)
+    # print elem.tag, elem.attrib['DownVotes']
+    for key,value in elem.attrib.iteritems():
+        print "%s    %s" % (key,value)
     print("")
 
 def fast_iter(context):
     ct = 0
-    print("starting fast iterator")
-    print("printing context...")
-    print(context)
+    # print("starting fast iterator")
+    # print("printing context...")
+    # print(context)
 
     for event, elem in context:
         if(ct > 10):
             break
-        print("count is %d" % ct)
+        #print("count is %d" % ct)
         ct += 1
 
-        print("about to call parse_block")       
+        #print("about to call parse_block")
         parse_block(elem)
         elem.clear()
         while elem.getprevious() is not None:
