f16512abf2c31f5d83fa659aae792f97e2be5424
Matt McNally <mgmcnally@umass.edu>
2015-11-11 16:34:20 -0500

    might sort almost work maybe a little

--- parse_xml.py
+++ parse_xml.py
@@ -1,1 +1,22 @@
 from lxml import etree
+
+infile = '/Users//Users/Matt/Desktop/stackexchange/Users.xml'
+context = etree.iterparse(infile, events=('end',), tag='Title')
+
+ct = 0
+
+fast_iter(context, parse_block)
+
+def parse_block(elem):
+    print("count is %d" % ct)
+    print(elem)
+    print("")
+    ct += 1
+
+def fast_iter(context, func):
+    for event, elem in context:
+        func(elem)
+        elem.clear()
+        while elem.getprevious() is not None:
+            del elem.getparent()[0]
+    del context
