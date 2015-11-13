10a00149e6d9b2182b9339bbb26091bb9636b934
Carter Tiernan <ctiernan@umass.edu>
2015-11-13 12:46:36 -0500

    partially working to populate db

--- data_dumper.py
+++ data_dumper.py
@@ -4,45 +4,63 @@ import os
 from collections import defaultdict
 import pprint
 import pickle
+import sqlite3
 
 # DATASET MUST BE IN SOURCE FOLDER WITHIN A FOLDER CALLED 'datasets'
 STATIC_PATH = os.getcwd()
 users = STATIC_PATH+'/datasets/Users.xml'
 posts = STATIC_PATH+'/datasets/Posts.xml'
 
-def get_question(elem, d):
+def get_question(elem, c):
     is_question = elem.attrib['PostTypeId'] is "1"
     has_accepted_answer = 'AcceptedAnswerId' in elem.attrib
 
     if is_question and has_accepted_answer:
-        q_id = elem.attrib['Id']
-        title = elem.attrib['Title']
-        body = elem.attrib['Body']
-        score = elem.attrib['Score']
-        views = elem.attrib['ViewCount']
-        accepted_answer_id = elem.attrib['AcceptedAnswerId']
+        q_id = int(elem.attrib['Id'])
+        title = str(elem.attrib['Title'])
+        body = str(elem.attrib['Body'])
+        score = int(elem.attrib['Score'])
+        views = int(elem.attrib['ViewCount'])
+        accepted_answer_id = int(elem.attrib['AcceptedAnswerId'])
 
+        q_cur = c.cursor()
+
+        q_cur.execute("INSERT INTO questions (id, title, body, score, views, acceptedanswerid) VALUES (1, 'f', 'd', 2, 3, 4)")
+        #q_cur.execute("INSERT INTO questions (id, title, body, score, views, acceptedanswerid) VALUES (%i, %s, %s, %i, %i, %i)" % (q_id, title, body, score, views, accepted_answer_id))
+        c.commit()
+
+        '''
         d[q_id]["title"] = accepted_answer_id
         d[q_id]["body"] = body
         d[q_id]["score"] = score
         d[q_id]["views"] = views
         d[q_id]["accepted_answer_id"] = accepted_answer_id
         d[q_id]["answers_ids"] = []
+        '''
 
-def get_answer(elem, d):
+def get_answer(elem, c):
     is_answer = elem.attrib['PostTypeId'] is "2"
 
     if is_answer:
-        a_id = elem.attrib['Id']
-        body = elem.attrib['Body']
-        score = elem.attrib['Score']
-        q_id = elem.attrib['ParentId']
+        a_id = int(elem.attrib['Id'])
+        body = str(elem.attrib['Body'])
+        score = str(elem.attrib['Score'])
+        q_id = int(elem.attrib['ParentId'])
 
+
+        a_cur = c.cursor()
+
+        a_cur.execute("INSERT INTO answers (id, body, score, pid) VALUES (%i, %s, %i, %i)" % (a_id, body, score, q_id))
+        c.commit()
+
+
+        '''
         d[a_id]["body"] = body
         d[a_id]["score"] = score
         d[a_id]["parent_id"] = q_id
+        '''
 
-def fast_iter(context, func, d, limit=None):
+def fast_iter(context, func, c, limit=None):
     ct = 0
     has_limit = limit is not None
 
@@ -53,7 +71,7 @@ def fast_iter(context, func, d, limit=None):
         ct += 1
         if ct % 100000 == 0:
             print "completed %i rows." % (ct)
-        func(elem, d)
+        func(elem,c)
         elem.clear()
 
         while elem.getprevious() is not None:
@@ -79,25 +97,30 @@ def delete_unused_answers(td,ad):
 
 context = etree.iterparse(posts)
 
+'''
 def dd():
     return defaultdict(int)
 questions_dict = defaultdict(dd) # dd is a module-level function
 answers_dict = defaultdict(dd) # dd is a module-level function
+'''
+conn = sqlite3.connect('db/datadump.db')
 
 print("getting questions...")
-fast_iter(context, get_question, questions_dict, limit=None)
-print("number of questions: %d" % len(questions_dict))
+fast_iter(context, get_question, conn, limit=10)
+# fast_iter(context, get_question, questions_dict, limit=None)
+# print("number of questions: %d" % len(questions_dict))
 
 print("getting answers...")
-fast_iter(context, get_answer, answers_dict, limit=None)
-print("number of answers pre-deleting unused: %d" % len(answers_dict))
+fast_iter(context, get_answer,  conn, limit=10)
+# fast_iter(context, get_answer, answers_dict, limit=None)
+# print("number of answers pre-deleting unused: %d" % len(answers_dict))
 
-to_del = link_dicts(questions_dict,answers_dict)
-delete_unused_answers(to_del,answers_dict)
-print("number of post-answers unused: %d" % len(answers_dict))
+# to_del = link_dicts(questions_dict,answers_dict)
+# delete_unused_answers(to_del,answers_dict)
+# print("number of post-answers unused: %d" % len(answers_dict))
 
-pickle.dump( questions_dict, open( "questions.p", "wb" ) )
-pickle.dump( answers_dict, open( "answers.p", "wb" ) )
+# pickle.dump( questions_dict, open( "questions.p", "wb" ) )
+# pickle.dump( answers_dict, open( "answers.p", "wb" ) )
 
 # print("")
 # pprint.pprint(questions_dict)
