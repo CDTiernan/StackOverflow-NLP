e92fda5f26c0b35b64d85f810deb593de611834b
Carter Tiernan <ctiernan@umass.edu>
2015-11-15 12:52:45 -0500

    i merged poorly and lost Matt's database creation functionality, I added it back and formatted it to work with how the db is populated now

--- data_dumper.py
+++ data_dumper.py
@@ -1,88 +1,7 @@
-# import importer as etree
 from lxml import etree
 import os
-# from collections import defaultdict
-# import pprint
-# import pickle
 import sqlite3
-import unicodedata
 
-# DATASET MUST BE IN SOURCE FOLDER WITHIN A FOLDER CALLED 'datasets'
-STATIC_PATH = os.getcwd()
-users = STATIC_PATH+'/datasets/Users.xml'
-posts = STATIC_PATH+'/datasets/Posts.xml'
-database = STATIC_PATH+'/db/datadump.db'
-
-
-def create_db_if_needed():
-    ''' CREATE DATABASE '''
-    if not os.path.exists(database):
-        # db doesn't exist
-
-        # connect to db (which creates it)
-        connection = sqlite3.connect('db/datadump.db')
-        cursor = connection.cursor()
-
-        # create questions table
-        cursor.execute("CREATE TABLE questions (id int, title string, body string, score int, views int, favorites int, comments int, acceptedanswerid int)")
-
-        # create answers table
-        cursor.execute("CREATE TABLE answers(id int, body string, score int, commentcount int, pid int, acceptedanswer boolean)")
-
-        connection.commit()
-        connection.close()
-
-
-def get_question(elem, c):
-    is_question = elem.attrib['PostTypeId'] is "1"
-    has_accepted_answer = 'AcceptedAnswerId' in elem.attrib
-
-    if is_question and has_accepted_answer:
-        q_id = int(elem.attrib['Id'])
-        # tilte unicodedata.normalize('NFKD', elem.attrib['Title']).encode('ascii','ignore')
-        title = elem.attrib['Title'].encode('ascii','ignore')
-        # tilte unicodedata.normalize('NFKD', elem.attrib['Body']).encode('ascii','ignore')
-        body = elem.attrib['Body'].encode('ascii','ignore')
-        score = int(elem.attrib['Score'])
-        views = int(elem.attrib['ViewCount'])
-        accepted_answer_id = int(elem.attrib['AcceptedAnswerId'])
-
-        q_cur = c.cursor()
-
-        q_cur.execute('INSERT INTO questions (id, title, body, score, views, acceptedanswerid) VALUES (?, ?, ?, ?, ?, ?)', (q_id, title, body, score, views, accepted_answer_id))
-        #q_cur.execute("INSERT INTO questions (id, title, body, score, views, acceptedanswerid) VALUES (%i, %s, %s, %i, %i, %i)" % (q_id, title, body, score, views, accepted_answer_id))
-        c.commit()
-
-        '''
-        d[q_id]["title"] = accepted_answer_id
-        d[q_id]["body"] = body
-        d[q_id]["score"] = score
-        d[q_id]["views"] = views
-        d[q_id]["accepted_answer_id"] = accepted_answer_id
-        d[q_id]["answers_ids"] = []
-        '''
-
-def get_answer(elem, c):
-    is_answer = elem.attrib['PostTypeId'] is "2"
-
-    if is_answer:
-        a_id = int(elem.attrib['Id'])
-        body = str(elem.attrib['Body']).encode('ascii','ignore')
-        score = int(elem.attrib['Score'])
-        q_id = int(elem.attrib['ParentId'])
-
-
-        a_cur = c.cursor()
-
-        a_cur.execute('INSERT INTO answers (id, body, score, pid) VALUES (?, ?, ?, ?)', (a_id, body, score, q_id))
-        c.commit()
-
-
-        '''
-        d[a_id]["body"] = body
-        d[a_id]["score"] = score
-        d[a_id]["parent_id"] = q_id
-        '''
 def populate_questions_answers_tables(elem, c):
 
     is_question = elem.attrib['PostTypeId'] is "1"
@@ -96,10 +15,8 @@ def populate_questions_answers_tables(elem, c):
         accepted_answer_id = int(elem.attrib['AcceptedAnswerId'])
 
         q_cur = c.cursor()
-
         q_cur.execute('INSERT INTO questions (id, title, body, score, views, acceptedanswerid) VALUES (?, ?, ?, ?, ?, ?)', (q_id, title, body, score, views, accepted_answer_id))
         c.commit()
-
 
     is_answer = elem.attrib['PostTypeId'] is "2"
     if is_answer:
@@ -109,21 +26,21 @@ def populate_questions_answers_tables(elem, c):
         q_id = int(elem.attrib['ParentId'])
 
         a_cur = c.cursor()
-
         a_cur.execute('INSERT INTO answers (id, body, score, pid) VALUES (?, ?, ?, ?)', (a_id, body, score, q_id))
         c.commit()
 
 def fast_iter(context, c, limit=None):
-    ct = 0
     has_limit = limit is not None
 
+    ct = 0
     for event, elem in context:
         if has_limit:
             if ct > limit:
                 break
+
         if ct % 50000 == 0:
             print "completed %i rows." % (ct)
-        # func(elem,c)
+
         populate_questions_answers_tables(elem, c)
         ct += 1
 
@@ -131,60 +48,49 @@ def fast_iter(context, c, limit=None):
 
         while elem.getprevious() is not None:
             del elem.getparent()[0]
+
+def create_db(c):
+        # connect to db (which creates it)
+        cur = c.cursor()
+
+        # create questions table
+        cur.execute("CREATE TABLE questions (id int, title string, body string, score int, views int, favorites int, comments int, acceptedanswerid int)")
+        # add indecies
+        cur.execute("CREATE INDEX qid_idx ON questions(id)")
+        cur.execute("CREATE INDEX acceptedanswerid_idx ON questions(acceptedanswerid)")
+        # create answers table
+        cur.execute("CREATE TABLE answers(id int, body string, score int, commentcount int, pid int, acceptedanswer boolean)")
+        # add indecies
+        cur.execute("CREATE INDEX aid_idx ON answers(id)");
+        cur.execute("CREATE INDEX pid_idx ON answers(id)");
+        cur.execute("CREATE INDEX isacceptedanswer_idx ON answers(acceptedanswer)");
+
+        c.commit()
+
+if __name__=='__main__':
+    # DATASET MUST BE IN SOURCE FOLDER WITHIN A FOLDER CALLED 'datasets'
+    static_path = os.getcwd()
+
+    users = static_path+'/datasets/Users.xml'
+    posts = static_path+'/datasets/Posts.xml'
+
+    # check to see if database exists
+    have_db = os.path.isfile('db/datadump.db')
+
+    # connect to the dataset
+    context = etree.iterparse(posts)
+    #connect to the database (and create it)
+    connection = sqlite3.connect('db/datadump.db')
+
+    # if db does not exist
+    if not have_db:
+        print("Creating and Connecting to DB")
+        create_db(connection)
+    else:
+        print("Connecting to DB")
+
+    print("Populating DB")
+    fast_iter(context, connection, limit=1000)
+
+    connection.close()
     del context
-'''
-def link_dicts(qd,ad):
-    answers_with_no_question = []
-    for a_id,info_dict in ad.iteritems():
-        q_id = info_dict["parent_id"]
-        if q_id in qd:
-            qd[q_id]["answers_ids"].append(a_id)
-            if qd[q_id]["accepted_answer_id"] == a_id:
-                ad[a_id]["is_accepted"] = 1
-        else:
-            answers_with_no_question.append(a_id)
-
-    return answers_with_no_question
-
-
-def delete_unused_answers(td,ad):
-    for a_id in td:
-        del ad[a_id]
-'''
-
-
-
-'''
-def dd():
-    return defaultdict(int)
-questions_dict = defaultdict(dd) # dd is a module-level function
-answers_dict = defaultdict(dd) # dd is a module-level function
-'''
-
-<<<<<<< HEAD
-print("populating questions and answers...")
-fast_iter(context, conn, limit=None)
-# fast_iter(context, get_question, questions_dict, limit=None)
-# print("number of questions: %d" % len(questions_dict))
-
-# print("getting answers...")
-# fast_iter(context, get_answer,  conn, limit=None)
-
-# cur = c.cursor()
-# cur.execute('SELECT count(id) from questions')
-
-# fast_iter(context, get_answer, answers_dict, limit=None)
-# print("number of answers pre-deleting unused: %d" % len(answers_dict))
-
-    # to_del = link_dicts(questions_dict,answers_dict)
-    # delete_unused_answers(to_del,answers_dict)
-    # print("number of post-answers unused: %d" % len(answers_dict))
-
-    # pickle.dump( questions_dict, open( "questions.p", "wb" ) )
-    # pickle.dump( answers_dict, open( "answers.p", "wb" ) )
-
-<<<<<<< HEAD
-# print("")
-# pprint.pprint(questions_dict)
-
-conn.close()
