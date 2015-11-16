c571dc5e044736c0a7591aa805774cb371182051
Matt McNally <mgmcnally@umass.edu>
2015-11-16 13:25:12 -0500

    added other sentiment analysis functions

--- simple_nlp.py
+++ simple_nlp.py
@@ -13,10 +13,15 @@ https://textblob.readthedocs.org/en/dev/quickstart.html#sentiment-analysis
 '''
 
 
-
+'''
+Returns True if sentiment of text is positive
+Since polarity ranges from 0 to 1, it is postive if the polarity is greater than 0.5
+'''
+def is_sentiment_positive(text):
+    return get_sentiment_polarity(text) > 0.5
 
 '''
-returns polarity of text between 0 and 1
+Returns polarity of text between 0 and 1
 0 is most negative
 1 is most positive
 '''
@@ -24,9 +29,17 @@ def get_sentiment_polarity(text):
     blob = TextBlob(text)
     return blob.sentiment.polarity
 
+'''
+Returns sentiment of text as a namedtuple of the form Sentiment(polarity, subjectivity)
+'''
+def get_sentiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment
+
+
 
 if __name__=='__main__':
     pos = get_sentiment_polarity("that answer is perfect. Worked flawlessly")
     neg = get_sentiment_polarity("I'm horrified that you would recommend that course of action")
-    print(pos)
-    print(neg)
+    print("polarity of positive string: %f" % pos)
+    print("polarity of negative string: %f" % neg)
